cc4f8767aa1aa9182cdbc7b4b111a4d776f2215c
AnthonyD9527 <106792017+AnthonyD9527@users.noreply.github.com>
2023-03-07 15:49:31 -0800

    Add files via upload

---
+++ functions_practice.py
@@ -0,0 +1,40 @@
+
+#Task 1
+
+
+def hello():
+    print("Hello, user!")
+
+
+
+#Task 2
+
+def pack(a,b,c):
+    return[a,b,c]
+
+
+
+#Task 3
+
+
+def eat_lunch(my_lst):
+  if len(my_lst) == 0:
+    print("My lunchbox is empty!")
+  else:
+    for i in range(len(my_lst)):
+      if i == 0:
+        print(f"First I eat {my_lst[0]}")
+      else:
+        print(f"Next I eat {my_lst[i]}")
+
+  
+
+
+
+
+hello()
+print(pack("a","b","c"))
+print(pack(1,2,3))
+eat_lunch([])
+eat_lunch(["sandwich"])
+eat_lunch(["apple","banana","sandwich","cookie"])
